d99ab8409a923074028aba8ef5517b0bc24dcdb3
Serega127 <132373671+Serega127@users.noreply.github.com>
2024-11-10 19:29:24 +0300

    Signed-off-by: Serega127 <132373671+Serega127@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import telebot
 from telebot import types
+import random
 
+
+words_dict = {}
 with open("words_coper.txt", "r", encoding='utf-8') as file:
     words_list = file.read().split('\n')[:-1]
-    words_dict = {}
     for i in words_list:
         word = i.split('$')
         words_dict[word[0]] = word[1] # words_dict = {'слово на русском': 'word on english'}
@@ -18,13 +20,21 @@ def start(message):
     keyboard = types.ReplyKeyboardMarkup()
     button1 = types.KeyboardButton('Пишем')
     keyboard.add(button1)
-    bot.reply_to(message, 'Привет! Я бот.', reply_markup=keyboard)
+    bot.reply_to(message, 'Бот работает так: он пишет слово на русском, а вы должны писать его на английском', reply_markup=keyboard)
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
+
+    def shuffle_dict(input_dict):
+        items = list(input_dict.items())
+        random.shuffle(items)
+        return dict(items)
+
+    words_dict2 = shuffle_dict(words_dict)
+    print(words_dict2)
     chat_id = message.chat.id
     if message.text == 'Пишем':
-        words = iter(words_dict.items())
+        words = iter(words_dict2.items())
         def write():
             try:
                 word, rigth_answer = next(words)
