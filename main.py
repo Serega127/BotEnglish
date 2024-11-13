46cc5fd55b6d51537a5ae1d24c39dea060f9a243
Serega127 <132373671+Serega127@users.noreply.github.com>
2024-11-13 16:47:53 +0300

    Добавил индексирование для словарей со словами

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import telebot
 from telebot import types
 import random
-import os
-import sys
-import time
 
-
-def craet_words():
+def creat_words():
     words_dict = {}
-    with open("words_coper.txt", "r", encoding='utf-8') as file:
+    with open("Words.txt", "r", encoding='utf-8') as file:
         words_list = file.read().split('\n')[:-1]
         for i in words_list:
             word = i.split('$')
@@ -22,14 +18,8 @@ def craet_words():
 
     return words_dict2
 
-words_dict = {}
-with open("words_coper.txt", "r", encoding='utf-8') as file:
-    words_list = file.read().split('\n')[:-1]
-    for i in words_list:
-        word = i.split('$')
-        words_dict[word[0]] = word[1] # words_dict = {'слово на русском': 'word on english'}
+users = {}
 
-print(words_dict)
 
 bot = telebot.TeleBot('7998808652:AAEXyWyGuo3gjfZyryQOIlm6FU73s0JgFn0')
 
@@ -43,60 +33,49 @@ def start(message):
 
 @bot.message_handler(func=lambda message: message.text == "Пишем")
 def handle_message(message):
-
-    def shuffle_dict(input_dict):
-        items = list(input_dict.items())
-        random.shuffle(items)
-        return dict(items)
-    words_dict2 = shuffle_dict(words_dict)
-    wrong_words = {}
-
     chat_id = message.chat.id
 
-    def write(dct):
-        if dct != {}:
+
+    words_dict = creat_words()
+    wrong_words = {}
+    users[chat_id] = [words_dict, wrong_words]
+
+
+    def write(func_chat_id):
+        print(users[func_chat_id][0])
+        if users[func_chat_id][0] != {}:
             def examination(message):
                 if message.text.lower() == '/stop':
                     bot.send_message(chat_id, 'Тест остановлен')
-                    return craet_words
+                    return creat_words
 
                 if message.text.lower() == rigth_answer:
                     bot.send_message(chat_id, f'🟢{rigth_answer}🟢')
-                    del dct[word]
-
+                    del users[func_chat_id][0][word]
                     try:
-                        del wrong_words[word]
+                        del users[func_chat_id][1][word]
                     except: None
-                    write(dct)
+                    write(chat_id)
                 else:
                     bot.send_message(chat_id, f'🔴{rigth_answer}🔴')
-                    del dct[word]
-                    wrong_words[word] = rigth_answer
-                    write(dct)
+                    del users[func_chat_id][0][word]
+                    users[func_chat_id][1][word] = rigth_answer
+                    write(chat_id)
 
-            word = next(iter(dct))
-            rigth_answer = dct[word]
+            word = next(iter(users[func_chat_id][0]))
+            rigth_answer = users[func_chat_id][0][word]
             msg = bot.send_message(chat_id, word)
             bot.register_next_step_handler(msg, examination)
 
-        if dct == {} and wrong_words == {}:
+        if users[func_chat_id][0] == {} and users[func_chat_id][1] == {}:
             bot.send_message(chat_id, 'Все слова верные, хорошая работа!')
 
-        if dct == {} and wrong_words != {}:
+        if users[func_chat_id][0] == {} and users[func_chat_id][1] != {}:
             bot.send_message(chat_id, 'Работа над ошибками')
-            words_dict2 = wrong_words.copy()
-            write(words_dict2)
+            users[func_chat_id][0], users[func_chat_id][1] = users[func_chat_id][1], users[func_chat_id][0]
+            write(chat_id)
 
-    write(words_dict2)
-
-
-@bot.message_handler(commands=['restart'])
-def restart(message):
-    chat_id = message.chat.id
-    bot.send_message(chat_id, "Бот перезапускается...")
-    bot.stop_polling()  # Завершаем текущий процесс polling
-    time.sleep(1)  # Небольшая пауза, чтобы завершить процессы
-    os.execv(sys.executable, [sys.executable] + sys.argv)  # Перезапускаем бот
+    write(chat_id)
 
 
 
